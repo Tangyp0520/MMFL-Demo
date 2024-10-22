22a0e2a69ad5a5d7ee35d57d417f7e79dd0c6963
tangyp <2445057315@qq.com>
2024-10-22 22:12:08 +0800

    修改思路补充

--- src/main.py
+++ src/main.py
@@ -8,9 +8,7 @@ args = parser.parse_args()
 
 if __name__ == '__main__':
     print("This is MMFL Demo")
-    # mnist_train_dataloader, mnist_test_dataloader = generate_dataloader('MNIST', 64)
-    # mnistm_train_dataloader, mnistm_test_dataloader = generate_dataloader('MNIST-M', 64)
     mmfl = MMFl()
     for epoch in range(100):
-        mmfl.train()
+        mmfl.train(epoch)
 
